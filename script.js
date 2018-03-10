8c513175d3faa199f293c86724996cb767b4bba1
antyoku <antyokunaoyasu@gmail.com>
2018-03-10 17:20:26 +0900

    feat: jQueryを（試しに）導入

---
+++ script.js
@@ -0,0 +1,10 @@
+$(function() {
+
+$("#footer__change").click(function(){
+    $(".header__list li").text("ーー");
+    $('body').css('background', 'url("imgs/town_b_c.jpg") center/cover no-repeat');
+})
+
+  
+
+});
