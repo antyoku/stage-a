3a855029727539bede40c2b11083666b93fa1789
antyoku <antyokunaoyasu@gmail.com>
2018-03-12 17:39:21 +0900

    jQuery追加（背景変更、効果音）

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,21 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('sass', function() {
+// Sassコンパイルタスク
+gulp.task('sass', function(){
   gulp.src('scss/**/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('css'));
 });
 
-gulp.task('watch', function () {
-    gulp.watch('scss/**/*.scss', ['sass']);
+// watchタスク(**/*.scss変更時に実行するタスク)
+gulp.task('sass-watch', ['sass'], function(){
+  var watcher = gulp.watch('scss/**/*.scss', ['sass']);
+  watcher.on('change', function(event) {
+    console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
   });
+});
+
+// gulpのデフォルト動作としてsass-watchを実行
+gulp.task('default', ['sass-watch']);
+  
--- script.js
+++ script.js
@@ -1,10 +1,76 @@
+var ImgCnt = 0;
+
 $(function() {
 
-$("#footer__change").click(function(){
-    $(".header__list li").text("ーー");
-    $('body').css('background', 'url("imgs/town_b_c.jpg") center/cover no-repeat');
+$(".header__list li").click(function(){
+    if (ImgCnt < 13) {
+    document.getElementById("Sound").currentTime = 0;
+    document.getElementById("Sound").play();
+    }
+    ImgCnt += 1;
+    switch (ImgCnt) {
+        case 14:
+        $(".header__list li").text("ーー");
+        $(".header__logo").text("ー");
+        $('#man').show();
+            break;
+        case 13:
+        $("#fall1").hide();$("#fall2").hide();$("#fall3").hide();$("#fall4").hide();$("#fall5").hide();
+        $('body').css('background', 'url("imgs/town_b_c.jpg") center/cover no-repeat');
+            break;
+        case 12:
+        setTimeout(function(){
+            $("#fall1").show().animate( {"top":"600px"}, { duration: 1500, easing: 'swing'});
+        },600);
+        setTimeout(function(){
+            $("#fall2").show().animate( {"top":"600px"}, { duration: 1600, easing: 'swing'});
+        },200);
+        setTimeout(function(){
+            $("#fall3").show().animate( {"top":"600px"}, { duration: 1200, easing: 'swing'});
+        },100);
+            setTimeout(function(){
+            $("#fall4").show().animate( {"top":"600px"}, { duration: 1700, easing: 'swing'});
+        },350);
+            setTimeout(function(){
+            $("#fall5").show().animate( {"top":"600px"}, { duration: 1100, easing: 'swing'});
+        },450);
+            break;
+        case 11:
+            $('body').css('background', 'url("imgs/town_b11.jpg") center/cover no-repeat');
+            break;
+        case 10:
+            $('body').css('background', 'url("imgs/town_b10.jpg") center/cover no-repeat');
+            break;
+        case 9:
+            $('body').css('background', 'url("imgs/town_b9.jpg") center/cover no-repeat');
+            break;
+        case 8:
+            $('body').css('background', 'url("imgs/town_b8.jpg") center/cover no-repeat');
+            break;
+        case 7:
+            $('body').css('background', 'url("imgs/town_b7.jpg") center/cover no-repeat');
+            break;
+        case 6:
+            $('body').css('background', 'url("imgs/town_b6.jpg") center/cover no-repeat');
+            break;
+        case 5:
+            $('body').css('background', 'url("imgs/town_b5.jpg") center/cover no-repeat');
+            break;
+        case 4:
+            $('body').css('background', 'url("imgs/town_b4.jpg") center/cover no-repeat');
+            break;
+        case 3:
+            $('body').css('background', 'url("imgs/town_b3.jpg") center/cover no-repeat');
+            break;
+        case 2:
+            $('body').css('background', 'url("imgs/town_b2.jpg") center/cover no-repeat');
+            break;
+        case 1:
+            $('body').css('background', 'url("imgs/town_b1.jpg") center/cover no-repeat');
+            break;
+        }
 })
 
-  
 
-});
+
+})
